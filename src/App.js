af19222947053ba3306335d51b40302e41ab7e0f
JanBegJan <janbeg2014@gmail.com>
2018-05-23 14:51:09 +0600

    clarifai api added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
+import FaceRecon from './components/FaceRecon/FaceRecon';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'c2a47be651f04df68a5a08c29171f6a1'
+ });
 
 const particleOptions = {
   particles: {
@@ -18,6 +24,28 @@ const particleOptions = {
   }
 }
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit =() =>{
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
   render() {
     return (
       <div className="App">
@@ -27,9 +55,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* 
-        <FaceRecon />} */}
+        <ImageLinkForm onInputChange= {this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecon />
       </div>
     );
   }
---
+++ src/components/FaceRecon/FaceRecon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const FaceRecon = () =>{
+return(
+  <div className='center'>
+    <p className='fa3  dim black underline pa3 pointer'>lorem</p>
+  </div>
+)
+}
+export default FaceRecon;
