76d6384d02dd1149bd9cec1d0f94eabc955341ca
JanBegJan <janbeg2014@gmail.com>
2018-05-26 18:17:59 +0600

    Fixed noopener

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -5,10 +5,10 @@ import React from 'react';
 
 const Copyright = () => {
   return (
-    <footer class="pv4 ph3 ph5-m ph6-l mid-gray">
-      <small class="f6 db tc">© 2018 <b class="ttu">JBJ Online</b>. All Rights Reserved</small>
-      <div class="tc mt1">
-        <p>Developed by <a target="_blank" href="https://twitter.com/janbegjan" title="Owner" class="f6 dib ph2 link mid-gray dim">@janbegjan</a></p>
+    <footer className="pv4 ph3 ph5-m ph6-l mid-gray">
+      <small className="f6 db tc">© 2018 <b className="ttu">JBJ Online</b>. All Rights Reserved</small>
+      <div className="tc mt1">
+        <p>Developed by <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/janbegjan" title="Owner" className="f6 dib ph2 link mid-gray dim">@janbegjan</a></p>
       </div>
     </footer>
   )
